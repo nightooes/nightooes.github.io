5e9dbda0da01ccd6933ae51998d08e378dd87178
nightooes <Kirya0112@yandex.ru>
2025-07-23 19:48:45 +0500

    Delete scripts directory

--- scripts/storage.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('saveBtn').addEventListener('click', () => {
-  const input = document.getElementById('numbersInput').value;
-  const numbers = input.split(/[, ]+/).filter(num => num.trim() !== '');
-  localStorage.setItem('savedNumbers', JSON.stringify(numbers));
-  alert('Список сохранен!');
-});
